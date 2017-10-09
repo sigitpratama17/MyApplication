7f39c23d060b0edbc651c135b06a1f617adf59a5
sigitpratama <lisdayuni02@gmail.com>
2017-10-09 17:40:24 +0700

    menambahkan myactivity.java dan haloo sigit hha pada activity_main.xml

--- app/src/main/java/monopro/inf/prak/myapplication/MainActivity1.java
+++ app/src/main/java/monopro/inf/prak/myapplication/MainActivity1.java
@@ -2,12 +2,16 @@ package monopro.inf.prak.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity1 extends AppCompatActivity {
-
+TextView texs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+
+        texs = (TextView) findViewById(R.id.teks);
     }
 }
